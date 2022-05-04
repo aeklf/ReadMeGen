555afb04f5e8125263308b5f9a684b5a5f903985
Andrea Kalife <andreaklfdlg@gmail.com>
2022-05-04 11:39:22 -0500

    Main JS

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require ("inquirer");
 const fs = require ("fs");
-const generateMarkdown = require ("./utils/generateMarkdown")
+const generateMarkdown = require ("./utils/generateMarkdown");
+const { type } = require("os");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+function userInput(){
+    return inquirer.prompt([
+    {name: "license",
+    message: "Select the type of license for this application:",
+    choices: ["Academic Free License v3.0", "Apache License 2.0", "Artistic License 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License" ]}, 
+    {name: "title",
+    message: "What is the name of your application?",
+    type: "input" },
+    {name: "description",
+    message: "What does your application do?",
+    type: "input"},
+    {name: "install",
+    message: "How is your application installed?",
+    type: "input"},
+    {name: "usage",
+    message: "How is your application used?",
+    type: "input"},
+    {name: "testing",
+    message: "How can your application be tested?",
+    type: "input"},
+    {name: "username",
+    message: "What is your GitHub username?",
+    type: "input"},
+    {name: "email",
+    message: "What is your e-mail?",
+    type: "input"},
+    {name: "fileName",
+    message: "What do you want to call your readme file?",
+    type: "input"},
+])};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data,
+    (err) => err ? console.error(err) : console.log(`${fileName}.md has been generated.`))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let answers = await userInput();
+    writeToFile((answers.fileName), (generateMarkdown(answers)));
+}
 
 // Function call to initialize app
 init();
